655120444cae48ccd14c78fd58bd01531b92aee6
Sara Kuandykova <kuandykova.sarah@gmail.com>
2024-01-11 22:41:02 +0500

    5/#8/добавила главную страницу

--- Beru.Rent/src/App.vue
+++ Beru.Rent/src/App.vue
@@ -1,21 +1,16 @@
 <template>
     <v-app>
         <Navbar />
-        <br/>
-        <br/>
-        <LoadPicture />
-        <router-view />
+        <router-view/>
     </v-app>
 </template>
 <script>
     import Navbar from "@/components/Navbar.vue";
-    import LoadPicture from "@/components/Forms/LoadPicture.vue";
 
     export default{
         name: 'App',
         components: {
-            Navbar,
-            LoadPicture
+            Navbar
         }
     };
 </script>
