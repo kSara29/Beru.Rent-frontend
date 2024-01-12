23239603103fb8d2b9b97f6f6290be7b000bfa90
Абдимансур Бауыржанулы <mansurbauyrzanly@gmail.com>
2024-01-12 20:18:49 +0600

    #9 добавил примерную страницу личного кабинета (доделаем когда появятся все поля, и авторизация)

--- Beru.Rent/src/App.vue
+++ Beru.Rent/src/App.vue
@@ -2,20 +2,16 @@
     <v-app>
         <Navbar />
         <br/>
-        <br/>
-        <LoadPicture />
         <router-view />
     </v-app>
 </template>
 <script>
     import Navbar from "@/components/Navbar.vue";
-    import LoadPicture from "@/components/Forms/LoadPicture.vue";
 
     export default{
         name: 'App',
         components: {
-            Navbar,
-            LoadPicture
+            Navbar
         }
     };
 </script>
