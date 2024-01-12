dc6a42257a8907f683aea85c41da907afbe046dc
Bauyrzhan Yerikov <bauka.erikov@gmail.com>
2024-01-12 18:51:46 +0600

    #6 доделан Navbar, добавлена кнопка поиска и кнопка выбора категорий.

--- Beru.Rent/src/components/Navbar.vue
+++ Beru.Rent/src/components/Navbar.vue
@@ -1,26 +1,72 @@
+
 <template>
     <v-container fluid>
-        <v-app-bar
-            color="black"
-            dark
-        >
-            <v-spacer/>
-            <v-btn outlined class="ml-3">
-                <router-link to="/">Главная страница</router-link>
-            </v-btn>
-            <v-btn outlined class="ml-3">
-                <router-link to="/register">Регистрация</router-link>
-            </v-btn>
+
+      <v-app-bar
+        color="teal-darken-4"
+        image="https://picsum.photos/1920/1080?random"
+        scroll-behavior="hide"
+      >
+        <template v-slot:image>
+          <v-img
+            gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+
+        <v-app-bar-title>
+          <v-btn prepend-icon="$vuetify">
+            <router-link to="/" style="text-decoration: none; color: inherit">Beru-Rent</router-link>
+          </v-btn>
+        </v-app-bar-title>
+
+        <!-- Поисковик -->
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-text-field
+          hide-details
+          single-line
+        ></v-text-field>
+
+        <!-- Категория -->
+        <v-app-bar-title style="align-self: self-start">
+          <v-select
+            clearable
+            label="Категория"
+            :items="['Спорт', 'Электроника', 'Инструменты', 'Спецтехника', 'Транспорт', 'Недвижимость']"
+          ></v-select>
+        </v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <!-- Создание объявления -->
+        <v-btn stacked outlined class="ml-3" >
+          <router-link style="text-decoration: none; color: inherit" to="/register">Создание объявления</router-link>
+        </v-btn>
+
+        <!-- Регистрация -->
+        <v-btn  stacked outlined class="ml-3" >
+          <router-link style="text-decoration: none; color: inherit" to="/register">Регистрация</router-link>
+        </v-btn>
+
+
+        <!-- Выход из аккаунт(после входа в аккаунт появляется) -->
+        <v-btn stacked>
+          <v-icon icon="mdi-login"></v-icon>
+          Выход
+        </v-btn>
+
         </v-app-bar>
-    </v-container>
-</template>
+      </v-container>
+  </template>
 
-<script>
-export default {
 
-}
-</script>
+  <script>
+  export default {
+  }
+  </script>
 
-<style scoped>
+  <style scoped>
 
-</style>
+  </style>
