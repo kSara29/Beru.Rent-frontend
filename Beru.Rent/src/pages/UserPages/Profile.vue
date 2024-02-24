9037a7b6be224bd243877da7d158bc9ac576babc
kuandykova.sarah <kuandykova.sarah@gmail.com>
2024-02-24 15:30:31 +0600

    изменила дизайн личного кабинета v2

--- Beru.Rent/src/pages/UserPages/Profile.vue
+++ Beru.Rent/src/pages/UserPages/Profile.vue
@@ -4,15 +4,11 @@
 
 <template>
   <div class="col-md-3"></div>
-      <v-card color="#FFCA28" height="100%">
+      <v-card color="#FFCA28" height="350px">
         <div style="display: flex;height: 100%;justify-content: center;">
           <div class="profile-main text" style="display: flex;align-content: stretch;flex-direction: column;align-items: center;">
-<!--            <div>
-              <v-img :src=this.avatar alt="Фото Юзера" style="border-radius: 50%; width:250px; height:250px;"></v-img>
-              <p class="user-info-title">{{ user.firstName }} {{ user.lastName }} </p>
-            </div>-->
             <div>
-              <img :src=this.avatar alt="Фото Юзера" style="border-radius: 50%;width:225px; height:225px;">
+              <img :src=this.avatar alt="Фото Юзера" style="border-radius: 50%;width:225px; height:225px; padding-top:20px">
               <p class="user-info-title">{{ user.firstName }} {{ user.lastName }} </p>
             </div>
             <div style="display:flex">
@@ -31,13 +27,26 @@
           <v-btn >
             <router-link style="text-decoration: none; color: inherit;" :to="'allChats'">Чаты</router-link>
           </v-btn>
-          <v-btn >
-            <router-link style="text-decoration: none; color: inherit;" :to="'allChats'">Сделки</router-link>
+          <v-btn @click="toggleGreeting">
+            <p >Сделки</p>
           </v-btn>
           <v-btn >
             <router-link style="text-decoration: none; color: inherit;" :to="'allChats'">Бронирования</router-link>
           </v-btn>
         </v-container>
+
+        <v-container v-if="showGreeting" class="mt-3">
+          <v-row justify="center">
+            <v-col cols="12">
+              <v-card>
+                <v-card-text>
+                  <h2>Deals list - Coming soon ...</h2>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+
         <v-container class="main-text" style="display:flex; justify-content:center">
           Мои объявления
         </v-container>
@@ -66,9 +75,13 @@ export default{
     avatar: '../../../public/fish.jpg',
     image: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22287%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20287%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18cfdf075ca%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18cfdf075ca%22%3E%3Crect%20width%3D%22287%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2299.91666412353516%22%20y%3D%2296.20000038146972%22%3EImage%20cap%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
     isUser: true,
-    ads: []
+    ads: [],
+    showGreeting: false
   }),
   methods: {
+    toggleGreeting() {
+      this.showGreeting = !this.showGreeting; // Переключение состояния блока с надписью "ПРИВЕТ"
+    },
     async get() {
       await axios.get(`http://localhost:5174/bff/user/getById`, {headers: {
         'accept': 'application/json',
@@ -113,6 +126,7 @@ export default{
   }
   .user-info-title{
     color: black;
+    padding-top:20px;
     font-size:25px;
   }
   .contacts {
